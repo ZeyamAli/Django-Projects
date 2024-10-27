7adb2fadfefc67bbd695832efb663958ccd5701f
Zeyam Ali <zeyamalimatharu@gmail.com>
2024-10-27 07:09:58 -0700

    book table created with attributes

--- booksApp/models.py
+++ booksApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    edition = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+
